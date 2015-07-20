2afcc3e7e214c3ddf58c6d435a7b9daf9eb5697e
brunovac <bruno.vacquerel@gmail.com>
2015-07-20 17:28:51 +0200

    V1 site
    Test index

--- index.php
+++ index.php
@@ -1,4 +1,315 @@
-<?php 
-  include("./includes/header.php"); 
- include("./includes/footer.php"); 
-?>
+<!DOCTYPE html>
+<html lang="en">  
+<head>
+    <title>Hockey Training Manager</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">    
+    <link rel="shortcut icon" href="favicon.ico">  
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'> 
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <!-- Plugins CSS -->    
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/plugins/prism/prism.css">
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
+</head> 
+
+<body data-spy="scroll">
+    
+    <!---// codeButtonFacebook -->
+    <!-- <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "URL ICI URL";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+     -->
+    <!-- ******HEADER****** --> 
+    <header id="header" class="header">  
+        <div class="container">            
+            <h1 class="logo pull-left">
+                <a class="scrollto" href="#promo">
+                    <span class="logo-title">HTM</span>
+                </a>
+            </h1><!--//logo-->              
+            <nav id="main-nav" class="main-nav navbar-right" role="navigation">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button><!--//nav-toggle-->
+                </div><!--//navbar-header-->            
+                <div class="navbar-collapse collapse" id="navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active nav-item sr-only"><a class="scrollto" href="#promo">Home</a></li>
+                        <li class="nav-item"><a class="scrollto" href="#apropos">A Propos</a></li>
+                        <li class="nav-item"><a class="scrollto" href="#technologies">Technologies</a></li>
+                        <li class="nav-item"><a class="scrollto" href="#plugin">Plugin</a></li>
+                        <li class="nav-item"><a class="scrollto" href="#langues">Langues/Equipe</a></li>                        
+                        <li class="nav-item last"><a class="scrollto" href="#contact">Contact</a></li>
+                    </ul><!--//nav-->
+                </div><!--//navabr-collapse-->
+            </nav><!--//main-nav-->
+        </div>
+    </header><!--//header-->
+    
+    <!-- ******PROMO****** -->
+    <section id="promo" class="promo section offset-header">
+        <div class="container text-center">
+            <h2 class="title">Hockey Training<span class="highlight">Manager</span></h2>
+            <p class="intro"> Une aide à la plannifcation d'entrainement de hockey, HTM est votre second entraineur adjoint.</p>
+            <div class="btns">
+                <a class="btn btn-cta-secondary" href="https://www.facebook.com/photo.php?fbid=751623911547843&set=t.1197025070&type=3&theater" target="_blank">Demo</a>
+                <a class="btn btn-cta-primary" href="assets/images/robin.jpg" target="_blank">Download</a>
+            </div>
+            <ul class="meta list-inline">
+                <li><a href="#" target="_blank">View on GitHub</a></li>
+                <li>Créé par : <a href="https://github.com/TecKnologikS" target="_blank">Robin Pauquet</a> et Bruno Vacquerel</li>
+            </ul>
+                </ul>
+            </div>
+        </div>
+    </section><!--//promo-->
+    
+    <!-- ******ABOUT****** --> 
+    <section id="apropos" class="about section">
+        <div class="container">
+            <h2 class="title text-center">Hockey Training Manager</h2>
+            <p class="intro text-center">HTM est un veritable entraineur adjoint virtuel, il prend en compte toutes les states concernant vos joueurs, et exploite au mieux possible.</p>
+            <div class="row">
+                <div class="item col-md-4 col-sm-6 col-xs-12">
+                    <div class="icon-holder">
+                        <i class="fa fa-heart"></i>
+                    </div>
+                    <div class="content">
+                        <h3 class="sub-title">Le Poste</h3>
+                        <p>Selon le poste des prédiléction de vos joueurs, HTM proposera différente séance d'entrainements, avec de spécialités différente, pour l'équipe, et chaque joueurs.</p>
+                    </div><!--//content-->
+                </div><!--//item-->
+                <div class="item col-md-4 col-sm-6 col-xs-12">
+                    <div class="icon-holder">
+                        <i class="fa fa-clock-o"></i>
+                    </div>
+                    <div class="content">
+                        <h3 class="sub-title">Il vous fait gagner du temps</h3>
+                        <p>En effet, grâce à la récupération des resultats en quasi-direct, il analyse dans les moindres details où vos joueurs en flanché ou excellé.</p>
+                    </div><!--//content-->
+                </div><!--//item-->
+                <div class="item col-md-4 col-sm-6 col-xs-12">
+                    <div class="icon-holder">
+                        <i class="fa fa-crosshairs"></i>
+                    </div>
+                    <div class="content">
+                        <h3 class="sub-title">Précis</h3>
+                        <p>HTM vous proposera non seulement des entrainements général par équipe, mais il va aussi vous conseiller des entrainements personnels et spécifiques pour chaque joueurs selon ces résultats. .</p>
+                    </div><!--//content-->
+                </div><!--//item-->           
+                <div class="clearfix visible-md"></div>    
+                <div class="item col-md-4 col-sm-6 col-xs-12">
+                    <div class="icon-holder">
+                        <!-- icon -->
+                    </div>
+                    <div class="content">
+                        <h3 class="sub-title">Lorem ipsum</h3>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div><!--//content-->
+                </div><!--//item-->                
+                <div class="item col-md-4 col-sm-6 col-xs-12">
+                    <div class="icon-holder">
+                        <!-- icon -->
+                    </div>
+                    <div class="content">
+                        <h3 class="sub-title">Lorem ipsum bacan</h3>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div><!--//content-->
+                </div><!--//item-->
+                <div class="item col-md-4 col-sm-6 col-xs-12">
+                    <div class="icon-holder">
+                        <!-- icon -->
+                    </div>
+                    <div class="content">
+                        <h3 class="sub-title">Bacon Ipsum Lorem</h3>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>               
+            </div>            
+        </div>
+    </section> 
+    <!-- ******FEATURES****** --> 
+    <section id="technologies" class="features section">
+        <div class="container text-center">
+            <h2 class="title">Features</h2>
+            <ul class="feature-list list-unstyled">
+                <li><i class="fa fa-check"></i> JAVA 8</li>
+                <li><i class="fa fa-check"></i> JAVA Swing</li>
+                <li><i class="fa fa-check"></i> HTML5 + CSS3</li>
+                <li><i class="fa fa-check"></i> Javascript</li>
+                <li><i class="fa fa-check"></i> Site contruit avec Bootstrap 3.3</li>
+                <li><i class="fa fa-check"></i> XML </li>
+            </ul>
+        </div><!--//container-->
+    </section><!--//features-->
+    
+    <!--*****DOCS******-->  
+    <section id="plugin" class="docs section">
+        <div class="container">
+            <div class="docs-inner">
+            <h2 class="title text-center">Choisie ton plugin pour HTM</h2>            
+            <div class="block">
+                <h3 class="sub-title text-center"></h3>
+                <p></p>
+                <p></p>
+                <ul class="list-unstyled">
+                    <li>Anglais</li>
+                    <li>Suédois</li>
+                    <li>Francais</li>
+                </ul>
+            </div>
+            lolilolioezjnofnzie test
+            <div class="block">
+                <h3 class="sub-title text-center">Ton Equipe </h3>
+               <p><a href="#">Bruins Boston</a> - <a href="#">Sabres Buffalo</a> - <a href="#">Red Wings Détroit</a> - <a href="#">Panthers Floride</a> - <a href="#">Canadiens de Montréal</a> - <a href="#">Sénateurs d'Ottawa</a> - <a href="#">Lightning de Tampa Bay</a> - <a href="#">Maple Leafs de Toronto</a> </p>
+
+                <p><a href="#">Hurricanes de la Caroline </a>- <a href="#">Blue Jackets de Columbus</a> - <a href="#">Devils du New Jersey</a> - <a href="#">Islanders de New York</a> - <a href="#">Rangers de New York</a> - <a href="#">Flyers de Philadelphie </a>- <a href="#">Penguins de Pittsburgh</a> - <a href="#">Capitals de Washington</a></p>
+
+                <P><a href="#">Blackhawks de Chicago</a> - <a href="#">Avalanche du Colorado</a> - <a href="#">Stars de Dallas</a> - <a href="#">Wild du Minnesota</a> - <a href="#">Predators de Nashville</a> - <a href="#">Blues de Saint-Louis</a> - <a href="#">Jets de Winnipeg</a></P>
+
+                <p><a href="#">Ducks d'Anaheim </a>- <a href="#">Flames de Calgary </a>- <a href="#">Oilers d'Edmonton </a>- <a href="#">Kings de Los Angeles </a>- <a href="#">Coyotes de Phoenix </a>- <a href="#">Sharks de San José</a> -<a href="#"> Canucks de Vancouver</a></p>
+                
+                <div class="code-block">
+                    <!--//Use Prismjs - http://prismjs.com/index.html#basic-usage
+                    <pre>
+    <code class="language-css">
+    /*   
+     * Template Name: devAid - Responsive Website Template for developers
+     * Version: 1.0
+     * Author: Xiaoying Riley at 3rd Wave Media
+     * Website: http://themes.3rdwavemedia.com/
+     * Twitter: @3rdwave_themes
+    */
+    body {
+      font-family: 'Lato', arial, sans-serif;
+      color: #444444;
+      font-size: 16px;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+    </code></pre>
+                </div><!--//code-block
+            </div><!--//block
+            
+            <div class="block">
+                <h3 class="sub-title text-center">JavaScript</h3>
+                <p>Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit ligula eget dolor.</p>
+                <div class="code-block">
+                    <pre><code class="language-javascript">
+    if( awesome ){
+        console.log('This is Awesome');
+    }else{
+        $('body').addClass('give-me-awesome');
+    }
+                    </code></pre>
+                </div><!--//code-block
+            </div><!--//block
+            <div class="block">
+                <h3 class="sub-title text-center">Full Documentation</h3>
+                <p>If your documentation is very long you can host the full docs page (including FAQ etc) on GitHub and provide a Call to Action button below to direct users there.</p>
+                <p class="text-center">
+                    <a class="btn btn-cta-primary" href="https://github.com/twbs/bootstrap" target="_blank">More on GitHub</a>
+                </p>
+            </div><!--//block
+            
+            </div><!--//docs-inner       
+        </div><!--//container-->
+    </section><!--//features-->
+    
+    <!-- ******LICENSE******
+    <section id="langues" class="license section">
+        <div class="container">
+            <div class="license-inner">
+            <h2 class="title text-center">Langues</h2>
+                <div class="info">
+                    <p>Hockey Training manager vous propose plusieurs langue pour le logiciel, afin d'accompagner les entraineurs, peu importe leurs pays d'excercice, leurs langue natale,...</p>
+                </div><!--//info
+                <div class="cta-container">
+                    <div class="speech-bubble">                    
+                        <p class="intro"></div>
+                    </div><!--//speech-bubble
+                </div><!--//cta-container-->
+            </div><!--//license-inner-->
+        </div><!--//container-->
+    </section><!--//how-->
+    
+    <!-- ******CONTACT****** --> 
+    <section id="contact" class="contact section has-pattern">
+        <div class="container">
+            <div class="contact-inner">
+                <h2 class="title  text-center">Contact</h2>
+                <p class="intro  text-center">En espérant que vous trouverez Hockey Training Manager utile, et efficase dans votre coaching. Pour toute suggestion n'hesitez pas à nous contacter</p>
+                <div class="author-message">                      
+                    <div class="profile">
+                        <img class="img-responsive" src="assets/images/robin.jpg" alt="" />
+                    </div><!--//profile-->
+                    <div class="speech-bubble">
+                        <h3 class="sub-title">Tu veux plus d'application</h3>
+                        <p>N'hesite pas a consulter nos different store 
+                        <p>Tu nous retrouveras sur <a href="https://play.google.com/store/apps/developer?id=TecKnologik%27S" target="_blank">Google PLay</a></p>
+                        <p>Ainsi que sur l'<a href="https://itunes.apple.com/us/artist/robin-pauquet/id935631690" target="_blank">Apple Store</a></p> 
+                        <div class="source">
+                            <span class="name"><a href="https://github.com/HockeyTrainingManager" target="_blank">Robin PAUQUET & Bruno VACQUEREL</a></span>
+                            <br />
+                            <span class="title">Java Developper</span>
+                        </div><!--//source-->
+                    </div><!--//speech-bubble-->                        
+                </div><!--//author-message-->
+                <div class="clearfix"></div>
+                <div class="info text-center">
+                    <h4 class="sub-title">Get Connected</h4>
+                    <ul class="social-icons list-inline">
+                        <li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="https://www.linkedin.com/"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="http://instagram.com/"><i class="fa fa-instagram"></i></a></li>  
+                        <li class="last"><a href="mailto:"><i class="fa fa-envelope"></i></a></li>              
+                    </ul>
+                </div><!--//info-->
+            </div><!--//contact-inner-->
+        </div><!--//container-->
+    </section><!--//contact-->  
+      
+    <!-- ******FOOTER****** --> 
+    <footer class="footer">
+        <div class="container text-center">
+            <small class="copyright">Designed with <i class="fa fa-heart"></i> by mamacita del palmas</small>
+        </div><!--//container-->
+    </footer><!--//footer-->
+     
+    <!-- Javascript -->          
+    <script type="text/javascript" src="assets/plugins/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/jquery-migrate-1.2.1.min.js"></script>    
+    <script type="text/javascript" src="assets/plugins/jquery.easing.1.3.js"></script>   
+    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>     
+    <script type="text/javascript" src="assets/plugins/jquery-scrollTo/jquery.scrollTo.min.js"></script> 
+    <script type="text/javascript" src="assets/plugins/prism/prism.js"></script>    
+    <script type="text/javascript" src="assets/js/main.js"></script>       
+</body>
+</html> 
+
+
+
+
+
+
+
+
+
